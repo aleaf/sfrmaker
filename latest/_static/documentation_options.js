bc2d05d1097c05af2fb8a591252f82d57ab78bdb
aleaf <andrew.t.leaf@gmail.com>
2026-06-23 15:46:47 +0000

    Deploying to gh-pages from @ aleaf/sfrmaker@ced6a9b9872aeaa2d6601f83a7737b1e9ae287e6 🚀

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.11.3.post70.dev0+g4c6df14',
+    VERSION: '0.11.3.post74.dev0+gced6a9b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
